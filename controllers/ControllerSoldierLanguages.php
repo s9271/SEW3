20035046fae4a468990396463e33ef0fabbb11a8
Mariusz <s9271@pjwstk.edu.pl>
2016-01-23 03:01:00 +0100

    FIX: Jezyki zolnierza

--- controllers/ControllerSoldierLanguages.php
+++ controllers/ControllerSoldierLanguages.php
@@ -1,5 +1,14 @@
 <?php
     class ControllerSoldierLanguages extends ControllerModel{
+        protected $using_top_title = true;
+        protected $top_ico = 'book';
+        
+        public function __construct(){
+            $this->breadcroumb = array(
+                array('name' => 'Żołnierze', 'link' => '/zolnierze')
+            );
+        }
+        
         // funkcja ktora jest pobierana w indexie, jest wymagana w kazdym kontrolerze!!!!!
         public function getContent(){
             return $this->getPage();
@@ -11,17 +20,24 @@
         // pobieranie strony
         protected function getPage()
         {
+            // tylul na pasku
+            $this->top_title = 'Lista znanych języków przez żołnierza';
+            
             // ladowanie klasy
             $item = new ClassSoldier(ClassTools::getValue('id_item'));
             
             // sprawdzanie czy klasa zostala poprawnie zaladowana
             if(!$item->load_class){
+                $this->tpl_values['wstecz'] = '/zolnierze';
                 $this->alerts['danger'] = 'Żołnierz nie istnieje';
                 
                 // ladowanie strony do wyswietlania bledow
                 // zmienne ktore mozna uzyc: wstecz, title oraz alertow
                 return $this->loadTemplate('alert');
             }
+            
+            $this->breadcroumb[] = array('name' => "{$item->name} {$item->surname}", 'link' => "/zolnierze/podglad/{$item->id}");
+            $this->breadcroumb[] = array('name' => "Języki", 'link' => "/zolnierze/{$item->id}/jezyki");
             
             return $this->getPageList($item);
         }
